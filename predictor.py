28a5d8642b0f73000f24e4a54a177d381ed14894
Nakatsuka Shunsuke <nakatsuka@cv.info.gifu-u.ac.jp>
2018-09-18 14:42:26 +0900

    :+1:

--- predictor.py
+++ predictor.py
@@ -39,31 +39,3 @@ class Predictor:
 
                 tile_results(os.path.join(logdir, 'batch_%d' % iter_),
                              x_real, x_eta, x_fake, diff, d_x)
-
-
-if __name__ == '__main__':
-    import sys
-    import yaml
-    from unet import UNet
-    from discriminator import Discriminator
-    from data_loader import Dataset
-
-    yml_path = sys.argv[1]
-    with open(yml_path) as f:
-        config = yaml.load(f)
-
-    if config['use_gpu']:
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
-
-    discriminator = Discriminator(**(config['discriminator_params']))
-    unet = UNet(**(config['unet_params']))
-
-    dl = Dataset(**(config['dataset_params'])) \
-        .flow_from_directory(**(config['dataloader_params']))
-
-    unet.load_state_dict(torch.load('../logs/debug/unet_20.pth'))
-    discriminator.load_state_dict(torch.load('../logs/debug/discriminator_20.pth'))
-
-    p = Predictor(unet, discriminator)
-
-    p(dl, './temp')
